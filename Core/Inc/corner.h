8fe9201efba42c90ab70ba8fd539952822684108
aluo005 <77523306+aluo005@users.noreply.github.com>
2026-01-17 14:15:00 -0600

    progress on polling spi data

--- Core/Inc/corner.h
+++ Core/Inc/corner.h
@@ -1,5 +1,7 @@
 #include "stm32f405xx.h"
 #include "stm32f4xx_hal_gpio.h"
+#include "virtual_timer.h"
+#include "corner_driver.h"
 
 // Position of the corner broker (front, left)
 enum CornerPosition {
@@ -12,4 +14,13 @@ enum CornerPosition {
 typedef struct {
     // Position of the corner board
     enum CornerPosition corner_pos;
+
+    // Timer group
+    TimerGroup *tg;
+
+    // hspi
+    SPI_HandleTypeDef* hspi;
+    uint8_t spi_buffer[4];
 } corners_;
+
+
--- Core/Src/corner.c
+++ Core/Src/corner.c
@@ -9,4 +9,26 @@ void initialize() {
     bool left = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_9);
 
     corners.corner_pos = (enum CornerPosition)((front << 1) | left);
+
+    VirtualTimer adc_timer = InitializeTimer(500, sg_loop);
+    VirtualTimer t2 = InitializeTimer(500, blank);
+    VirtualTimer t3 = InitializeTimer(500, blank);
+    VirtualTimer t4 = InitializeTimer(500, blank);
+    VirtualTimer t5 = InitializeTimer(500, blank);
+    TimerGroup timers[5] = {adc_timer, t2, t3, t4, t5};
+    corners.tg = InitializeTimerGroup(timers);
 }
+
+void sg_loop() {
+    bool drdy_pin = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6);
+
+    if (drdy_pin == GPIO_PIN_RESET) { // When DRDY pin drives low, begin shifting data
+        Read_ADC_Data(corners.hspi, corners.spi_buffer);
+    }
+
+    uint32_t raw = (corners.spi_buffer[0] << 16) | (corners.spi_buffer[1] << 8) | (corners.spi_buffer[2]);
+
+    
+}
+
+void blank(){}
