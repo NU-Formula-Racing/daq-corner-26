21b3620420da206480494fdd311152e060bb6836
aluo005 <77523306+aluo005@users.noreply.github.com>
2026-05-18 23:14:48 -0500

    debug name change header file

--- Core/Inc/corner.h
+++ Core/Inc/corner.h
@@ -19,8 +19,8 @@ void initialize(SPI_HandleTypeDef* hspi, CAN_HandleTypeDef* hcan, I2C_HandleType
                 ADC_HandleTypeDef* hadc);
 void tire_temp_group();
 void temp_loop();
-void print_group();
-void main_loop();
+void print_loop();
+void event_loop();
 void SG_Receive_Data();
 
 void initQueue();
